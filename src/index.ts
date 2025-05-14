8f6fc728869ea84ae6af6ff2ab915b052ec14ac0
Javier Valero Cejudo <javalce29@gmail.com>
2025-05-14 20:35:18 +0200

    feat: implement main command structure with signal handling

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello, world!');
+import { Command } from 'commander';
+
+import { description, name, version } from '../package.json';
+
+import { init } from './commands/init';
+
+process.on('SIGINT', () => process.exit(0));
+process.on('SIGTERM', () => process.exit(0));
+
+function main(): void {
+  const program = new Command()
+    .name(name)
+    .description(description)
+    .helpOption('-h, --help', 'Display this help message')
+    .version(version, '-v, --version', 'Display the current version');
+
+  program.addCommand(init);
+
+  program.parse();
+}
+
+main();
