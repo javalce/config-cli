bf3528133f8847cc714fe6ee8c88007cb86fa77c
Javier Valero Cejudo <javalce29@gmail.com>
2025-05-14 20:26:15 +0200

    build: update tsup configuration to include shims and retain dts option

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
+  shims: true,
   format: 'esm',
-  dts: true,
-  outDir: 'dist',
   clean: true,
+  dts: true,
   minify: true,
 });
