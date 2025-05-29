a9c0fa2d6b2bfd4567d4806ca786ab6ad311cac3
Javier Valero Cejudo <javalce29@gmail.com>
2025-05-29 18:51:42 +0200

    build: remove shims and dts options from tsup configuration

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   entry: ['src/index.ts'],
-  shims: true,
   format: 'esm',
   clean: true,
-  dts: true,
   minify: true,
+  banner: {
+    js: '#!/usr/bin/env node',
+  },
 });
